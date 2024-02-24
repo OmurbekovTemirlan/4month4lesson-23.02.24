6a539929ee7749a9a7ff07e15e831ddf9314ff41
OmurbekovTemirlan <Japarhanovaldik@gmail.com>
2024-02-24 22:31:30 +0600

    commit finish

---
+++ 4month4lessonDZ/Views/DetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailViewController.swift
+//  4month4lessonDZ
+//
+//  Created by Apple on 23.2.2024.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
